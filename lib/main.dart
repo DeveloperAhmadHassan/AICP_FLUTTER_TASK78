f4c56a4e2e9330c7db8695e1b5301de01bd59914
DeveloperAhmadHassan <ahmad.hassantariq921@gmail.com>
2024-07-12 17:18:11 +0500

    "Improve Code"

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:aicp_internship/Task1/ToDoApp.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
