5a5fcd799a92ff46175f774110e49276c262427e
DeveloperAhmadHassan <ahmad.hassantariq921@gmail.com>
2024-08-01 20:38:05 +0500

    first commit

--- lib/Task3/GlobalThemeData.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class GlobalThemData {
-  static ThemeData themeData(ColorScheme colorScheme, Color focusColor) {
-    return ThemeData(colorScheme: colorScheme, focusColor: focusColor);
-  }
-}
